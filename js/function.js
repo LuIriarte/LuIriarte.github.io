19562c42a1d4293274ec9b5d0b9f627239840ed3
LuIriarte <125817009+LuIriarte@users.noreply.github.com>
2023-03-20 09:25:19 -0300

    initial commit

---
+++ js/function.js
@@ -0,0 +1,41 @@
+let menuVisible = false;
+
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
+
+function seleccionar(){
+    
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
+
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("htmlcss");
+        habilidades[2].classList.add("rstudio");
+        habilidades[3].classList.add("python");
+        habilidades[4].classList.add("powerbi");
+        habilidades[5].classList.add("comunicacion");
+        habilidades[6].classList.add("trabajo");
+        habilidades[7].classList.add("creatividad");
+        habilidades[8].classList.add("dedicacion");
+       
+    }
+}
+
+
+
+window.onscroll = function(){
+    efectoHabilidades();
+} 
